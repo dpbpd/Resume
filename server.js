d0f5f58f024f845a543af45b37fabb58d72db500
David Bergstrom <david@Davids-MacBook-Pro.local>
2019-10-18 14:40:31 -0500

    css not found fix 4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use('/static', express.static('public'))
+app.use('/public', express.static('public'))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
